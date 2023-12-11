628e0dc6c21d626d1d6cde237ed51c5b185df79f
ETHEVE Fabien <fabienetheve@hotmail.com>
2023-12-11 13:58:55 +0100

    ajustement des paramètres de la card

--- src/components/BigCard/BigCard.svelte
+++ src/components/BigCard/BigCard.svelte
@@ -6,6 +6,6 @@
 <div class="w-1/3 bg-white rounded shadow-md">
 <figure>
     <img src="{picture}" alt="{name}">
-    <figcaption class="py-4 px-2"><div class="flex justify-between items-center"> {name || 'Bonjour toi'} <button class="bg-primary w-20 text-white rounded p-2 text-xs ">Voir</button></div></figcaption>
+    <figcaption class="py-4 px-2"><div class="flex justify-between items-center"><h3 class="text-base">{name} </h3><button class="bg-primary w-20 text-white rounded p-2 text-xs ">Voir</button></div></figcaption>
 </figure>
 </div>
