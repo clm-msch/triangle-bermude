43d53255080a1d7a2c07816cb297aa536a78b7c4
clement <58054176+clm-msch@users.noreply.github.com>
2023-12-11 13:39:04 +0100

    ✨ Create Searchbar component files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script>
     import '../app.css'
+    import SearchBar from '../components/SearchBar/SearchBar.svelte'
 </script>
 
 
 <main class="container m-auto h-screen">
-
+    <SearchBar />
 </main>
