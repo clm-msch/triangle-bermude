62292586662dcae557319af45c0ed64a15f7c7da
ETHEVE Fabien <fabienetheve@hotmail.com>
2023-12-11 13:51:07 +0100

    AJout des big cards

---
+++ src/components/BigCard/BigCard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let picture: string
+    export let name: string
+</script>
+
+<div class="w-1/3 bg-white rounded shadow-md">
+<figure>
+    <img src="{picture}" alt="{name}">
+    <figcaption class="py-4 px-2"><div class="flex justify-between items-center"> {name || 'Bonjour toi'} <button class="bg-primary w-20 text-white rounded p-2 text-xs ">Voir</button></div></figcaption>
+</figure>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script>
     import '../app.css'
+    import BigCard from '../components/BigCard/BigCard.svelte';
 </script>
 
 
 <main class="container m-auto h-screen">
-
+<BigCard picture="https://cdn.paris.fr/qfapv4/2023/11/08/huge-d30b641eefa7803154c41c459f67f336.jpg" name="Fontaine au roi"/>
 </main>
