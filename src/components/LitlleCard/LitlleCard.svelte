7b648d0b125e8759b5d8db0c2361c334e9835353
ETHEVE Fabien <fabienetheve@hotmail.com>
2023-12-11 16:00:17 +0100

    Création des petites cartes

---
+++ src/components/LitlleCard/LitlleCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+export let picture:string
+export let name:string
+export let description:string
+export let price:string
+
+</script>
+
+<div class="w-1/4">
+<figure>
+    <img src="{picture}" alt="{name}">
+    <figcaption>
+        {@html description}
+        <span>{price}</span>
+    </figcaption>
+</figure>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
     import '../app.css'
 import CarousselCard from '../components/CarousselCard/CarousselCard.svelte';
+import LitlleCard from '../components/LitlleCard/LitlleCard.svelte';
 </script>
 
 
 
 <main class="container m-auto h-screen">
     <CarousselCard />
+    <LitlleCard picture="https://cdn.paris.fr/qfapv4/2023/11/10/huge-cd431e415bc99704c599a5c88d3994bd.jpg" name="Paris sport proximité: tennis de table au gymnase Blanqui" description="<p>Venez pratiquer une activitée sportive proche de chez vous! Rejoignez-nous chaque mardi pour participer à des sessions de <strong>tennis de table </strong>au<strong> gymnase</strong> <strong>Blanqui</strong>. </p>" price="gratuit"/>
 </main>
