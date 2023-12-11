13cef61a02b4694bdda1b31380c73a62227c4a83
ETHEVE Fabien <fabienetheve@hotmail.com>
2023-12-11 15:46:12 +0100

    initialisation du caroussel

--- src/components/BigCard/BigCard.svelte
+++ src/components/BigCard/BigCard.svelte
@@ -3,9 +3,9 @@
     export let name: string
 </script>
 
-<div class="w-1/3 bg-white rounded shadow-md">
-<figure>
-    <img src="{picture}" alt="{name}">
-    <figcaption class="py-4 px-2"><div class="flex justify-between items-center"><h3 class="text-base">{name} </h3><button class="bg-primary w-20 text-white rounded p-2 text-xs ">Voir</button></div></figcaption>
+<div class="w-1/2 bg-white rounded-md shadow-md  mx-2">
+<figure >
+    <img src="{picture}" alt="{name}" class="rounded-t-md">
+    <figcaption class="py-4 px-2"><div class="flex justify-between items-center"><h3 class="text-base">{name} </h3><button class="bg-primary w-20 text-white rounded-md p-2 text-xs ">Découvrir</button></div></figcaption>
 </figure>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script>
     import '../app.css'
-    import BigCard from '../components/BigCard/BigCard.svelte';
+import CarousselCard from '../components/CarousselCard/CarousselCard.svelte';
 </script>
 
 
+
 <main class="container m-auto h-screen">
-<BigCard picture="https://cdn.paris.fr/qfapv4/2023/11/08/huge-d30b641eefa7803154c41c459f67f336.jpg" name="Fontaine au roi"/>
+    <CarousselCard />
 </main>
