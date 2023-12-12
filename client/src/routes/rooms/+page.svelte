3e9f7572b34ec90857b1408a49770d8a12f6919f
ETHEVE Fabien <fabienetheve@hotmail.com>
2023-12-12 16:36:29 +0100

    ajout de la page zoom

--- client/src/routes/rooms/+page.svelte
+++ client/src/routes/rooms/+page.svelte
@@ -10,8 +10,8 @@ import Gallery from '../../components/Gallery/Gallery.svelte';
     </div>
     <hr>
     <Gallery picture='https://cdn.paris.fr/qfapv4/2023/07/26/huge-7a54e7be61474eacf645a10190fd5e10.jpg' name="Arc de Triomphe"/>
-    <section class>
-
+    <section class='flex flex-col'>
+        <h2>A propos</h2>
     </section>
 
 </main>
