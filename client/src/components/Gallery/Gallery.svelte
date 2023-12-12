f01c10636e822b7aa66e81f15e1f0ee8dc2b9949
ETHEVE Fabien <fabienetheve@hotmail.com>
2023-12-12 16:30:37 +0100

    commencement de la page zoom

---
+++ client/src/components/Gallery/Gallery.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  
+  export let picture: string;
+  export let name: string;
+
+ 
+</script>
+
+<div class="grid grid-cols-3 mt-6 gap-4 h-96">
+  <div class="col-span-2 mr-2 row-span-2">
+    <img src={picture} alt={name} class="rounded-l-xl" />
+  </div>
+  <div id="map" class="row-span-2" style="height: 100%;"></div>
+</div>
