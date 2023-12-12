2173b2583707ad99e0d9a0586acfe89fdd723766
ETHEVE Fabien <fabienetheve@hotmail.com>
2023-12-12 16:51:27 +0100

    mise en place des icon des dates et de l'acces des personnes

--- client/src/components/Gallery/Gallery.svelte
+++ client/src/components/Gallery/Gallery.svelte
@@ -6,9 +6,9 @@
  
 </script>
 
-<div class="grid grid-cols-3 mt-6 gap-4 h-96">
+<div class="grid grid-cols-3 mt-6 gap-4 ">
   <div class="col-span-2 mr-2 row-span-2">
     <img src={picture} alt={name} class="rounded-l-xl" />
   </div>
-  <div id="map" class="row-span-2" style="height: 100%;"></div>
+  <div id="map" class="row-span-2" ></div>
 </div>
