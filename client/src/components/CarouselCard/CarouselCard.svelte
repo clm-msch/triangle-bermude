316e2367e79b58b0c919137a511551f59ca662e9
clement <58054176+clm-msch@users.noreply.github.com>
2023-12-14 10:31:47 +0100

    Add @splidejs/svelte-splide package and import CarouselCard component

---
+++ client/src/components/CarouselCard/CarouselCard.svelte
@@ -0,0 +1,14 @@
+<script>
+import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+import BigCard from '../BigCard/BigCard.svelte';
+</script>
+<div class="container m-auto w-fit h-fit">
+  <Splide aria-label="My Favorite Images">
+    <SplideSlide>
+      <BigCard picture="" name="" />
+    </SplideSlide>
+    <SplideSlide>
+      <BigCard picture="" name="" />
+    </SplideSlide>
+  </Splide>
+</div>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
     import '../app.css'
     import Header from '../layout/Header/Header.svelte'
+    import CarouselCard from '../components/CarouselCard/CarouselCard.svelte'
     import Footer from '../layout/Footer/Footer.svelte'
 </script>
 <title>Unic Paris</title>
 
 <main>
     <Header />
+    <CarouselCard />
     <Footer />
 </main>
