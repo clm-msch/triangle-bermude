47695b506741653d5e66bc4786b4e499c634fa41
ETHEVE Fabien <fabienetheve@hotmail.com>
2023-12-14 15:53:53 +0100

    pokijhgfd

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+import '../app.css'
+import CarousselCard from '../components/CarousselCard/CarousselCard.svelte';
+import LitlleCard from '../components/LitlleCard/LitlleCard.svelte';
+</script>
+
+
+
+<main class="container m-auto h-screen">
+   
+    
+   
+</main>
