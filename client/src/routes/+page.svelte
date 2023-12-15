044d1ef31d4a4054e39bc534d06c237c99947cf5
scotty974 <fabienetheve@hotmail.com>
2023-12-15 13:52:38 +0100

    error merging okokay

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,62 +1,96 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import '../app.css'
-    import Header from '../layout/Header/Header.svelte'
-    import BigCard from '../components/BigCard/BigCard.svelte'
-    import CarouselBigCard from '../components/CarouselBigCard/CarouselBigCard.svelte';
-    import Footer from '../layout/Footer/Footer.svelte'
-    import LittleCard from '../components/LitlleCard/LitlleCard.svelte'
-    import CardActivities from '../components/CardActivities/CardActivities.svelte';
-    let datas = []
-    let wordData:string=''
-    const api = 'https://shuhado.alwaysdata.net/django/api/v1/event/'
-
-    async function fetchData(){
-    
-    try{
-        const resp = await fetch(api)
-        const data:any = await resp.json()
-        datas = data
-        console.log(datas)
-        return data
-
-    }catch(error){
-        console.log(error)
+    import { onMount,afterUpdate  } from "svelte";
+    import "../app.css";
+    import Header from "../layout/Header/Header.svelte";
+    import LitlleCard from "../components/LitlleCard/LitlleCard.svelte";
+    import CarouselBigCard from "../components/CarouselBigCard/CarouselBigCard.svelte";
+    import Footer from "../layout/Footer/Footer.svelte";
+  
+    import CardActivities from "../components/CardActivities/CardActivities.svelte";
+    let datas = [];
+    let wordData: string = "";
+    const api = "https://shuhado.alwaysdata.net/django/api/v1/event/";
+  
+    async function fetchData() {
+      try {
+        let apiUrl = api;
+  
+        // Si un terme de recherche est défini, ajoutez-le à l'URL de l'API
+        if (wordData) {
+          apiUrl += `?search=${wordData}`;
+        }
+  
+        const resp = await fetch(apiUrl);
+        const data = await resp.json();
+  if(wordData){
+      console.log(data)
+  }
+        // Assurez-vous de traiter correctement les résultats en fonction de la présence du terme de recherche
+        datas = wordData ? data.results : data;
+  
+       
+        return datas;
+      } catch (error) {
+        console.log(error);
+      }
     }
-}
-        onMount(async ()=>{
-        fetchData()
-    })
-function handleSearch(event:any){
-    wordData = event.detail;
-    console.log(wordData)
-}
-   
-</script>
-<title>Unic Paris</title>
-
-<main>
-    
+    onMount(async () => {
+      await fetchData();
+    });
+    function handleSearch(event: any) {
+      wordData = event.detail;
+      // Fetch data based on the search term
+      fetchData();
+    }
+  
+    $: fetchData();
+  
+  // Utilisez afterUpdate pour gérer les mises à jour après chaque rendu
+  afterUpdate(() => {
+    console.log('Data updated');
+  });
+  </script>
+  
+  <title>Unic Paris</title>
+  
+  <main>
     <div class="m-2 sm:m-6 flex flex-col gap-6 sm:gap-12">
-        <!-- <div class="p-6">
-            {#each datas as data}
-            <LittleCard picture='{data.cover_url}' name='{data.title}' description='{data.cover_url}' price='{data.cover_url}' on:click={() => handleId(123)} />
-            {/each}
-        </div> -->
-        <Header on:word="{handleSearch}"/>
+      <!-- <div class="p-6">
+              {#each datas as data}
+              <LittleCard picture='{data.cover_url}' name='{data.title}' description='{data.cover_url}' price='{data.cover_url}' on:click={() => handleId(123)} />
+              {/each}
+          </div> -->
+      <Header on:word={handleSearch} />
+      {#if !wordData}
         <div class="flex flex-col gap-2 sm:gap-6">
-            {wordData}
-            <h2 class="font-brand font-semibold text-2xl sm:text-3xl text-secondary">Les Nouveautés</h2>
-            <CarouselBigCard />
-            <div class="flex flex-col mt-6">
-                <h2 class="font-brand font-semibold text-2xl sm:text-3xl text-secondary">Dernière Chance</h2>
-           <p class="font-basic font-light">Profitez de ces activités avant qu'elle ne s'arrête.</p>
-            </div>
-                <CardActivities /> 
-            
+          <h2
+            class="font-brand font-semibold text-2xl sm:text-3xl text-secondary"
+          >
+            Les Nouveautés
+          </h2>
+          <CarouselBigCard />
+          <div class="flex flex-col mt-6">
+            <h2 class="font-brand font-semibold text-2xl sm:text-3xl text-secondary">Dernière Chance</h2>
+            <p class="font-basic font-light">Profitez de ces activités avant qu'elle ne s'arrête.</p>
+          </div>
+          <CardActivities />
         </div>
-        <!-- <BigCard picture="https://cdn.paris.fr/qfapv4/2023/10/23/huge-efc64f2a6bd0674e013a2a946ce6f8cd.jpg" name="Pétanque square Emile Chautemps" /> -->
-        <!-- <CarouselCard /> -->
-        <Footer />
+      {:else}
+        {#each datas as data}
+        {data.results.title}
+          <LitlleCard
+            picture={data.cover_url}
+            name={data.title}
+            description={data.lead_text}
+            price={data.price_id}
+            id={data.id}
+          />
+        {/each}
+      {/if}
+  
+      <!-- <BigCard picture="https://cdn.paris.fr/qfapv4/2023/10/23/huge-efc64f2a6bd0674e013a2a946ce6f8cd.jpg" name="Pétanque square Emile Chautemps" /> -->
+      <!-- <CarouselCard /> -->
+      <Footer />
     </div>
-</main>
+  </main>
+  
