7c85eac0d516fa0d1d9d98f8eed686f6809bd124
clement <58054176+clm-msch@users.noreply.github.com>
2023-12-15 10:31:50 +0100

    Add API integration and event data fetching

--- client/src/components/CarouselBigCard/CarouselBigCard.svelte
+++ client/src/components/CarouselBigCard/CarouselBigCard.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  // import Carousel from 'svelte-carousel';
   import BigCard from '../BigCard/BigCard.svelte';
 
   let datas: any[] = [];
@@ -21,22 +20,14 @@
     await fetchData();
   });
 
-  // let carousel: any;
-  // const handleNextClick = () => {
-  //   carousel.goToNext();
-  // };
+  function handleId(id:any){
+    console.log(id)
+}
 </script>
 
-<!-- <Carousel
-  bind:this={carousel}
-  let:loaded
-  autoplay
-  autoplayDuration={3000}
-  autoplayProgressVisible
-> -->
   <div class="grid grid-cols-2 sm:grid-cols-3 gap-4">
   {#each datas.slice(0, 6) as data (data.id)}
     <BigCard picture="{data.cover_url}" name="{data.title}" />
   {/each}
 </div>
-<!-- </Carousel> -->
+
